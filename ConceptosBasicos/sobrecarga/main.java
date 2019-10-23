c195bb585c7c8d53c7009a57f9fc6f41a0507b45
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-23 01:49:37 -0500

    Create main.java

---
+++ ConceptosBasicos/sobrecarga/main.java
@@ -0,0 +1,21 @@
+package AtributosYobjetos;
+
+public class main {
+    public static void main(String[] args) {
+        
+        
+        Persona p1 = new Persona("Alexis",24); 
+        p1.correr();
+        p1.correr(52);
+        
+        
+        Persona p2 = new Persona("1581606");
+        
+       
+        
+        
+        
+        
+    }
+    
+}
