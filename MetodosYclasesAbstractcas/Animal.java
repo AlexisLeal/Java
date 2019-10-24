a07b6c98fc0a233fc8fe27d9b1f3dcceb9d48a73
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 17:18:14 -0500

    Create Animal.java

---
+++ MetodosYclasesAbstractcas/Animal.java
@@ -0,0 +1,8 @@
+package Paquete1;
+
+
+public abstract class Animal extends Servivo{
+        
+    
+    //como animal es abstacto no hay necesidad de implemtar el metodo como se hizo en la clsae Planta 
+}
