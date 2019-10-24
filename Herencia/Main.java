37e3b07ea204395ea2a63744b8beb798c8c5062f
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 13:19:29 -0500

    Create Main.java

---
+++ Herencia/Main.java
@@ -0,0 +1,16 @@
+package Herencia;
+public class Main {
+    
+    
+    public static void main(String[] args){
+        
+        Estudiante estudiante = new Estudiante("Alexis","Leal",24,1581606,90);
+        
+        
+        estudiante.mostrarDatos();
+        
+        
+        
+    }
+    
+}
