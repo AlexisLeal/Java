96722b79c61795adee0f056b3a56ccd53922aa91
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 17:17:23 -0500

    Create AnimalHerbivoro.java

---
+++ MetodosYclasesAbstractcas/AnimalHerbivoro.java
@@ -0,0 +1,14 @@
+package Paquete1;
+public class AnimalHerbivoro extends Servivo{
+    
+    @Override
+    public void alimentarse(){
+        System.out.println("Come plantas");
+    }
+    
+
+    
+    
+    
+    
+}
