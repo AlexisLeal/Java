9571d190fe108ceaae83854595572b18e6f7ceeb
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 14:49:35 -0500

    Create Perro.java

---
+++ SobreEscrituraMetodos/Perro.java
@@ -0,0 +1,11 @@
+package SobreEscritura;
+
+
+public class Perro extends Animal {
+    
+    @Override
+    public void comer(){
+        System.out.println("Estoy comiendo en mi platico en el suelo");
+    }
+    
+}
