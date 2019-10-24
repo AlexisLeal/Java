e28fe38fde4f807719972693ed912ddad8d84f7c
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 17:17:51 -0500

    Create AnimalCarnivoro.java

---
+++ MetodosYclasesAbstractcas/AnimalCarnivoro.java
@@ -0,0 +1,13 @@
+package Paquete1;
+
+
+public class AnimalCarnivoro extends Servivo{
+    
+    
+    @Override
+    public void alimentarse(){
+        System.out.println("El animal come carne");
+        
+    }
+    
+}
