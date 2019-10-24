2b63b115282a7d764361296ed4e9b8856767c300
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 17:13:51 -0500

    Create Planta.java

---
+++ MetodosYclasesAbstractcas/Planta.java
@@ -0,0 +1,12 @@
+package Paquete1;
+
+public class Planta extends Servivo{
+    
+    
+    //Como herreda un metodo abstracto si o si se tiene que implementar o sobreescirbise 
+    @Override
+    public void alimentarse(){
+        System.out.println("La planta se alimneta mediante la fotosintesis");
+    }
+    
+}
