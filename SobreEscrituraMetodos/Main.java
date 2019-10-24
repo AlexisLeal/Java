88bb3e61eeb96d9ee4c77e16e06f32ba028ecf78
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 14:50:10 -0500

    Create Main.java

---
+++ SobreEscrituraMetodos/Main.java
@@ -0,0 +1,20 @@
+package SobreEscritura;
+
+
+public class Main {
+    
+    
+    public static void main(String [] args){
+        
+        Animal ani = new Animal();
+        Persona p = new Persona();
+        Perro pe = new Perro();
+        
+        ani.comer();
+        p.comer();
+        pe.comer();
+        
+        
+    }
+    
+}
