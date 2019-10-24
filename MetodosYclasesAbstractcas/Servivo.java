d0fc4d99549dddfdba21e374a90ac2874042a299
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-24 17:13:16 -0500

    Create Servivo.java

---
+++ MetodosYclasesAbstractcas/Servivo.java
@@ -0,0 +1,20 @@
+package Paquete1;
+public abstract class Servivo {
+    
+    
+    
+    
+    public abstract void alimentarse();
+    //Le debemos poner la palabra reserbada abstract
+    
+    //SOLO SE DEFINE PARA IMPLEMENTAR TIENES QUE ABRIR Y CERRAR LLAVES 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
