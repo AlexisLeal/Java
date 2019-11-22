409071a091f151d2fa1a26eea8d7ebf15afc1d97
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-11-21 20:00:17 -0600

    Create Casting.java

---
+++ Casting/Casting.java
@@ -0,0 +1,21 @@
+Casting 
+
+
+double numero1= 7.5;
+
+
+int numero = (int) nunmero1;
+
+
+//-------------------------
+
+
+PaqueteEnvio paqueteRecibido;
+            
+paqueteRecibido=(PaqueteEnvio) flujoentrada.readObject(); //es igual a paqueteRecibido=flujoentrada.readObject();
+
+
+
+//El primero es el objeto 
+//El segundo es a lo que queremos convertir 
+// por ultimo a lo que queremos igualar 
