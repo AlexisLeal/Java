e5184fd5f2a1484eb69ddaedb039a86d47cde1a1
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-11-21 20:14:38 -0600

    Update Casting.java

--- Casting/Casting.java
+++ Casting/Casting.java
@@ -15,6 +15,11 @@ PaqueteEnvio paqueteRecibido;
 paqueteRecibido=(PaqueteEnvio) flujoentrada.readObject(); //es igual a paqueteRecibido=flujoentrada.readObject();
 
 
+public final int suma(int a, int b){ //el final es que ya no se puede heredar 
+            return a+b;
+}
+
+
 
 //El primero es el objeto 
 //El segundo es a lo que queremos convertir 
